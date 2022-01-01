023c8a63eb3b4a33d25170ca39da9e0e39970e7e
Edys Meza <edysmeza@gmail.com>
2021-12-31 22:21:31 -0600

    clean up settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,24 +1,6 @@
 <?php
 
-/**
- * Load services definition file.
- */
-$settings['container_yamls'][] = __DIR__ . '/services.yml';
-
-
-/**
- * If there is a local settings file, then include it
- */
-$local_settings = __DIR__ . "/settings.local.php";
-if (file_exists($local_settings)) {
-  include $local_settings;
-}
-
-// Automatically generated include for settings managed by ddev.
-$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
-if (is_readable($ddev_settings) && getenv('IS_DDEV_PROJECT') == 'true') {
-  require $ddev_settings;
-}
+$settings['hash_salt'] = 'ZaqwtvcpMtYuitAQwEHspJqfyMkcOtxlWEbZapqdIzdToVSkVcmalxBLEPhLlOQR';
 
 $databases['default']['default'] = array (
   'database' => 'sites/default/files/.ht.sqlite',
