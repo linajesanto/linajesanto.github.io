77b6b9b271cc7db972cf0b6efc76edaa65928d54
linajesanto <96889768+linajesanto@users.noreply.github.com>
2022-02-04 17:14:08 -0600

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,12 +5,12 @@ set -e
 drush cr
 drush tome:static -y -l https://edysmp.github.io/linajesanto
 rm -rf gh-pages
-git clone git@github.com:edysmp/linajesanto gh-pages
+git clone git@github.com:linajesanto/linajesanto.github.io gh-pages
 cd gh-pages
 git checkout main || git checkout -b main
 cd ..
 rm -rf gh-pages/*
-cp -r html/linajesanto/* gh-pages/
+cp -r html/* gh-pages/
 cd gh-pages
 git add .
 git commit -m 'Updating gh-pages site'
